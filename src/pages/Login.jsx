8246771ce898c84cbab02491bcf4a7cc4b7c2054
tomasmenecesfernandez <tomasmeneces170@gmail.com>
2026-09-11 00:18:53 +0000

    .

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -29,7 +29,7 @@ function Login() {
                         "El nombre completo tiene que tener como mínimo 6 letras",
                     );
                 }
-                if (!email.includes("@") || !email.includes(".")) {
+                if (!email.includes("@") || !email.includes(".") || !email.includes("com")) {
                     throw new Error("El correo no es válido");
                 }
                 if (contraseña.length < 6) {
